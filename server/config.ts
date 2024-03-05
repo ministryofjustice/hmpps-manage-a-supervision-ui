4d610b19c62111f102c0205d72182200fc772fe4
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-03-05 11:52:20 +0000

    PI-1880: Fixed env variable

--- server/config.ts
+++ server/config.ts
@@ -85,14 +85,6 @@ export default {
       agent: new AgentConfig(Number(get('TOKEN_VERIFICATION_API_TIMEOUT_RESPONSE', 5000))),
       enabled: get('TOKEN_VERIFICATION_ENABLED', 'false') === 'true',
     },
-    deliusIntegration: {
-      url: get('DELIUS_INTEGRATION_URL', 'http://localhost:8100', requiredInProduction),
-      timeout: {
-        response: Number(get('DELIUS_INTEGRATION_TIMEOUT_RESPONSE', 5000)),
-        deadline: Number(get('DELIUS_INTEGRATION_TIMEOUT_DEADLINE', 5000)),
-      },
-      agent: new AgentConfig(Number(get('DELIUS_INTEGRATION_TIMEOUT_RESPONSE', 5000))),
-    },
     masApi: {
       url: get('MAS_API_URL', 'http://localhost:8100', requiredInProduction),
       timeout: {

b9a0d19b251f48b983c4a1acc25e8acf6544b5c1
Marcus Aspin <maspin@unilink.com>
2024-03-20 12:11:25 +0000

    PI-1954 Add ZAP scanning

---
+++ .zap/traverse.js
@@ -0,0 +1,11 @@
+function listChildren(node, level) {
+  var j
+  for (j = 0; j < node.getChildCount(); j++) {
+    print(Array(level + 1).join('    ') + node.getChildAt(j).getNodeName())
+    listChildren(node.getChildAt(j), level + 1)
+  }
+}
+
+root = model.getSession().getSiteTree().getRoot()
+
+listChildren(root, 0)

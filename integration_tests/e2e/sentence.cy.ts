33ebb6fb2bef98476cc6bc3865de3b3456c7cdf8
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-03-22 16:18:09 +0000

    PI-1939 - refactor and update cypress test

--- integration_tests/e2e/sentence.cy.ts
+++ integration_tests/e2e/sentence.cy.ts
@@ -9,15 +9,20 @@ context('Sentence', () => {
     page.headerName().should('contain.text', 'Eula Schmeler')
     page.pageHeading().should('contain.text', 'Sentence: 3')
     page.pageHeading().should('contain.text', 'Sentence: 1')
+
     page.getTab('overview').should('contain.text', 'Overview')
     page.getTab('personalDetails').should('contain.text', 'Personal details')
     page.getTab('risk').should('contain.text', 'Risk')
     page.getTab('sentence').should('contain.text', 'Sentence')
     page.getTab('activityLog').should('contain.text', 'Activity log')
     page.getTab('compliance').should('contain.text', 'Compliance')
+
     page.getCardHeader('offence').should('contain.text', 'Offence')
     page.getCardHeader('offence').should('have.length', 2)
     page.getCardHeader('conviction').should('contain.text', 'Conviction')
     page.getCardHeader('conviction').should('have.length', 2)
+
+    page.getRowDataIndex('offence', 'dateOfOffence', 'Value', 0).should('contain.text', '20 March 2024')
+    page.getRowDataIndex('offence', 'dateOfOffence', 'Value', 1).should('contain.text', '20 January 2024')
   })
 })

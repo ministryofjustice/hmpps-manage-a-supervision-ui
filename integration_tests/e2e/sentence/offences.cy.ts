a5cbbbd47d0882a50771aa495d7d1212cabcc2e6
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-04-24 09:22:54 +0100

    PI-2000 update e2e test

--- integration_tests/e2e/sentence/offences.cy.ts
+++ integration_tests/e2e/sentence/offences.cy.ts
@@ -1,7 +1,21 @@
 context('Sentence', () => {
   it('Sentence page is rendered', () => {
     cy.visit('/case/X000001/sentence/offences/3')
+
     cy.get('h1').contains('Offences')
     cy.get('h2').contains('Additional offences')
+
+    cy.get('section')
+      .eq(0)
+      .within(() => cy.get('h2').contains('Main offence'))
+    cy.get('section')
+      .eq(0)
+      .within(() => cy.get('dt').eq(0).contains('Offence'))
+    cy.get('section')
+      .eq(0)
+      .within(() => cy.get('dt').eq(1).contains('Category'))
+    cy.get('section')
+      .eq(0)
+      .within(() => cy.get('dt').eq(2).contains('Notes'))
   })
 })

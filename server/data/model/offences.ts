22bca8b98ada0de6b59a265c143d142dba4a0e84
achimber-moj <161360519+achimber-moj@users.noreply.github.com>
2024-04-25 14:48:30 +0100

    Feature/pi 2000 mas sentence tab offences (#54)
    * PI-2000 add ui components for offences page
    
    * PI-2000 update page
    
    * PI-2000 update page
    
    * PI-2000 refactor variable names
    
    * PI-2000 update e2e test
    
    * PI-2000 update e2e tests
    
    * PI-2000 refactor following b/e updates
    
    * PI-2000 refactor tests, due to code duplication warning in sonar

---
+++ server/data/model/offences.ts
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/no-cycle
+
+import { Name } from './common'
+
+export interface Offences {
+  name: Name
+  mainOffence: Offence
+  mainOffenceNotes: string
+  additionalOffences: Offence[]
+}
+
+export interface Offence {
+  description: string
+  category: string
+  code: string
+  dateOfOffence: string
+}

ae2f445629302e912b3e87e59f82c158815c4f47
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2024-05-07 15:21:03 +0100

    PI-2152: Default to team when 1 team and add links when no team (#68)

--- integration_tests/pages/index.ts
+++ integration_tests/pages/index.ts
@@ -2,7 +2,7 @@ import Page, { PageElement } from './page'
 
 export default class IndexPage extends Page {
   constructor() {
-    super('Manage a Supervision')
+    super('Your cases')
   }
 
   headerUserName = (): PageElement => cy.get('[data-qa=header-user-name]')

dcaa9cfbdb352393519f41d2d1e4b705c83e8a5b
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2024-07-12 09:41:34 +0100

    PI-2167: Added ROLE_MANAGE_SUPERVISIONS (#97)
    * PI-2167: Added ROLE_MANAGE_SUPERVISIONS

--- server/middleware/authorisationMiddleware.test.ts
+++ server/middleware/authorisationMiddleware.test.ts
@@ -7,7 +7,7 @@ function createToken(authorities: string[]) {
   const payload = {
     user_name: 'USER1',
     scope: ['read', 'write'],
-    auth_source: 'nomis',
+    auth_source: 'delius',
     authorities,
     jti: 'a610a10-cca6-41db-985f-e87efb303aaf',
     client_id: 'clientid',

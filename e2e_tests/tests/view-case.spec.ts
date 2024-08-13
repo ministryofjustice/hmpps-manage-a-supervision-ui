489dd0215ed2d94f55761caf437353c0a2fddc26
Marcus Aspin <maspin@unilink.com>
2024-08-13 12:34:39 +0000

    Re-use shared steps from e2e project + add CI workflow

--- e2e_tests/tests/view-case.spec.ts
+++ e2e_tests/tests/view-case.spec.ts
@@ -1,13 +1,15 @@
 import { expect, test } from '@playwright/test'
 import * as dotenv from 'dotenv'
+import { DateTime } from 'luxon'
 import { deliusPerson } from '@ministryofjustice/hmpps-probation-integration-e2e-tests/steps/delius/utils/person'
 import { createOffender } from '@ministryofjustice/hmpps-probation-integration-e2e-tests/steps/delius/offender/create-offender'
 import { login as loginToDelius } from '@ministryofjustice/hmpps-probation-integration-e2e-tests/steps/delius/login'
-import loginToManageMySupervision from '../steps/login'
+import { login as loginToManageMySupervision } from '@ministryofjustice/hmpps-probation-integration-e2e-tests/steps/manage-a-supervision/login'
 
-dotenv.config() // read environment variables into process.env
+dotenv.config({ path: 'e2e_tests/.env' }) // read environment variables into process.env
 
-test('Verify the persons header details in Manage a Supervision', async ({ page }) => {
+test('Verify the header details', async ({ page }) => {
+  // Create a person in Delius
   await loginToDelius(page)
   const person = deliusPerson()
   const crn = await createOffender(page, { person })
@@ -23,6 +25,8 @@ test('Verify the persons header details in Manage a Supervision', async ({ page 
   await expect(page).toHaveTitle('Manage a Supervision - Overview')
   await expect(page.locator('[data-qa="crn"]')).toContainText(crn)
   await expect(page.locator('[data-qa="name"]')).toContainText(`${person.firstName} ${person.lastName}`)
-  await expect(page.locator('[data-qa="headerDateOfBirthValue"]')).toContainText('27 October 1961')
+  await expect(page.locator('[data-qa="headerDateOfBirthValue"]')).toContainText(
+    DateTime.fromJSDate(person.dob).toFormat('d MMMM yyyy'),
+  )
   await expect(page.locator('[data-qa="tierValue"]').first()).toContainText('D0')
 })
--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: process.env.CI ? 'on-first-retry' : 'off',
+    trace: process.env.CI ? 'off' : 'on',
   },
 
   /* Configure projects for major browsers */

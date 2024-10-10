090bdd3e4e1d7de7ac3041703b00b42e3ec08001
achimber-moj <161360519+achimber-moj@users.noreply.github.com>
2024-10-10 09:23:21 +0100

    MAN-23 - add missing acceptance criteria (#171)
    * MAN-23 - add missing acceptance criteria
    
    * MAN-23 - add missing acceptance criteria
    
    * MAN-23 - update unit test
    
    * MAN-23 - update unit test

--- integration_tests/e2e/recentCases.cy.ts
+++ integration_tests/e2e/recentCases.cy.ts
@@ -5,17 +5,27 @@ context('Recent Cases', () => {
   it('Recent Cases page is rendered', () => {
     cy.visit('/recent-cases', {
       onBeforeLoad(win) {
-        const recentCases = []
-        const recentCase = {
-          name: 'Wolff,Caroline',
-          crn: 'X000001',
-          dob: '9 January 2002',
-          age: 22,
-          tierScore: 'B2',
-          sentence: '12 month Community order',
-        }
+        const recentCases = [
+          {
+            name: 'Wolff,Caroline',
+            crn: 'X000001',
+            dob: '9 January 2002',
+            age: 22,
+            tierScore: 'B2',
+            numberOfAdditionalSentences: '1',
+            sentence: '12 month Community order',
+          },
+          {
+            name: 'Berge,Alton',
+            crn: 'X778160',
+            dob: '12 March 2012',
+            age: 12,
+            tierScore: 'A3',
+            numberOfAdditionalSentences: '0',
+            sentence: 'CJA - Std Determinate Custody',
+          },
+        ]
 
-        recentCases.push(recentCase)
         win.localStorage.setItem('recentCases', JSON.stringify(recentCases))
       },
     })
@@ -46,11 +56,21 @@ context('Recent Cases', () => {
     cy.get('@row1col2').within(() => cy.contains('9 January 2002'))
     cy.get('@row1col2').within(() => cy.contains('span', 22))
 
-    cy.get('tbody')
-      .eq(0)
-      .within(() => cy.get('td').eq(2).should('contain.text', 'B2'))
-    cy.get('tbody')
-      .eq(0)
-      .within(() => cy.get('td').eq(3).should('contain.text', '12 month Community order'))
+    cy.get('tbody').within(() => cy.get('td').eq(2).should('contain.text', 'B2'))
+    cy.get('tbody').within(() => cy.get('td').eq(3).should('contain.text', '12 month Community order'))
+
+    cy.get('tbody').within(() =>
+      cy
+        .get('tr')
+        .eq(0)
+        .within(() => cy.get('a').eq(1).invoke('attr', 'href').should('equal', './case/X000001/sentence')),
+    )
+
+    cy.get('tbody').within(() =>
+      cy
+        .get('tr')
+        .eq(1)
+        .within(() => cy.get('td').eq(3).should('contain.text', 'CJA - Std Determinate Custody')),
+    )
   })
 })

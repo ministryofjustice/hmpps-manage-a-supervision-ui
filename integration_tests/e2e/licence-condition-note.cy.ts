5d30048986bb2d44d45a4e07325fdefb77870b3a
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-10-21 14:16:01 +0100

    MAN-28 - update integration test

--- integration_tests/e2e/licence-condition-note.cy.ts
+++ integration_tests/e2e/licence-condition-note.cy.ts
@@ -14,12 +14,35 @@ context('Sentence', () => {
       cy.get('h2').should('contain.text', 'Alcohol Monitoring (Electronic Monitoring)'),
     )
 
+    cy.get(`[class=app-summary-card__body]`).within(() => cy.get('dt').should('have.length', 5))
+
+    cy.get(`[class=app-summary-card__body]`).within(() => cy.get('dd').should('have.length', 5))
+
     cy.get(`[class=app-summary-card__body]`).within(() =>
       cy.get('dt').eq(0).should('contain.text', 'Imposed (Release) date'),
     )
+    cy.get(`[class=app-summary-card__body]`).within(() => cy.get('dd').eq(0).should('contain.text', '25 December 2024'))
 
     cy.get(`[class=app-summary-card__body]`).within(() =>
       cy.get('dt').eq(1).should('contain.text', 'Actual start date'),
+    )
+    cy.get(`[class=app-summary-card__body]`).within(() => cy.get('dd').eq(1).should('contain.text', '26 December 2024'))
+
+    cy.get(`[class=app-summary-card__body]`).within(() => cy.get('dt').eq(2).should('contain.text', 'Note added by'))
+    cy.get(`[class=app-summary-card__body]`).within(() => cy.get('dd').eq(2).should('contain.text', 'CVL Service'))
+
+    cy.get(`[class=app-summary-card__body]`).within(() => cy.get('dt').eq(3).should('contain.text', 'Date added'))
+    cy.get(`[class=app-summary-card__body]`).within(() => cy.get('dd').eq(3).should('contain.text', '22 April 2024'))
+
+    cy.get(`[class=app-summary-card__body]`).within(() => cy.get('dt').eq(4).should('contain.text', 'Note'))
+    cy.get(`[class=app-summary-card__body]`).within(() =>
+      cy
+        .get('dd')
+        .eq(4)
+        .should(
+          'contain.text',
+          'Licence Condition created automatically from the Create and Vary a licence system of\\nAllow person(s) as designated by your supervising officer to install an electronic monitoring tag on you and access to install any associated equipment in your property, and for the purpose of ensuring that equipment is functioning correctly. You must not damage or tamper with these devices and ensure that the tag is charged, and report to your supervising officer and the EM provider immediately if the tag or the associated equipment are not working correctly. This will be for the purpose of monitoring your alcohol abstinence licence condition(s) unless otherwise authorised by your supervising officer. Licence Condition created automatically from the Create and Vary a licence system of\\nAllow person(s) as designated by your supervising officer to install an electronic monitoring tag on you and access to install any associated equipment in your property, and for the purpose of ensuring that equipment is functioning correctly. You must not damage or tamper with these devices and ensure that the tag is charged, and report to your supervising officer and the EM provider immediately if the tag or the associated equipment are not working correctly. This will be for the purpose of monitoring your alcohol abstinence licence condition(s) unless otherwise authorised by your supervising officer.Licence Condition created automatically from the Create and Vary a licence system of\\nAllow person(s) as desi123456\n',
+        ),
     )
   })
 })

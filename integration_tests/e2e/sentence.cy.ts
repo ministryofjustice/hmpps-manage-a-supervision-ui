a43136c4ef59dd1ddb6a4e89d11ae59c8b144901
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-11-05 17:40:52 +0000

    MAN-21 - refactor sentence page

--- integration_tests/e2e/sentence.cy.ts
+++ integration_tests/e2e/sentence.cy.ts
@@ -16,62 +16,37 @@ context('Sentence', () => {
     page.getTab('activityLog').should('contain.text', 'Activity log')
     page.getTab('compliance').should('contain.text', 'Compliance')
 
+    cy.get('[class="moj-side-navigation__item moj-side-navigation__item--active"]').within(() =>
+      cy.get('a').invoke('attr', 'href').should('equal', '/case/X000001/sentence?3'),
+    )
+
+    cy.get('[class="moj-side-navigation__item"]')
+      .eq(0)
+      .within(() => cy.get('a').invoke('attr', 'href').should('equal', '/case/X000001/sentence?1'))
+
+    cy.get('[class="moj-side-navigation__item"]')
+      .eq(1)
+      .within(() => cy.get('a').invoke('attr', 'href').should('equal', '/case/X000001/sentence/probation-history'))
+
     page.getCardHeader('offence').should('contain.text', 'Offence')
-    page.getCardHeader('offence').should('have.length', 2)
     page.getCardHeader('conviction').should('contain.text', 'Conviction')
-    page.getCardHeader('conviction').should('have.length', 2)
     page.getCardHeader('sentence').should('contain.text', 'Sentence')
-    page.getCardHeader('sentence').should('have.length', 2)
-    page.getCardHeader('probationHistory').should('contain.text', 'Previous probation details')
-    page.getCardHeader('probationHistory').should('have.length', 1)
 
-    page.getRowDataIndex('offence', 'mainOffence', 'Value', 0).should('contain.text', 'Murder (3 count)')
-    page.getRowDataIndex('offence', 'mainOffence', 'Value', 1).should('contain.text', 'Another Murder (1 count)')
-
-    page.getRowDataIndex('offence', 'dateOfOffence', 'Value', 0).should('contain.text', '20 March 2024')
-    page.getRowDataIndex('offence', 'dateOfOffence', 'Value', 1).should('contain.text', '20 January 2024')
-
-    page.getRowDataIndex('offence', 'offenceNotes', 'Value', 0).should('contain.text', 'overview')
-    page
-      .getCardHeader('offence')
-      .eq(1)
-      .within(() => cy.get('.govuk-summary-list__value').eq(2).should('contain.text', 'No notes'))
-
-    page.getRowDataIndex('offence', 'additionalOffences', 'Value', 0).should('contain.text', 'Burglary (2 count)')
-    page.getRowDataIndex('offence', 'additionalOffences', 'Value', 0).should('contain.text', 'Assault (1 count)')
-    page
-      .getCardHeader('offence')
-      .eq(1)
-      .within(() => cy.get('.govuk-summary-list__value').eq(3).should('contain.text', 'No additional offences'))
-
-    page.getRowDataIndex('conviction', 'sentencingCourt', 'Value', 0).should('contain.text', 'Hull Court')
-    page
-      .getCardHeader('conviction')
-      .eq(1)
-      .within(() => cy.get('.govuk-summary-list__value').eq(0).should('contain.text', 'No court details'))
-
-    page.getRowDataIndex('conviction', 'responsibleCourt', 'Value', 0).should('contain.text', 'Birmingham Court')
-    page
-      .getCardHeader('conviction')
-      .eq(1)
-      .within(() => cy.get('.govuk-summary-list__value').eq(1).should('contain.text', 'No court details'))
-
-    page.getRowDataIndex('conviction', 'convictionDate', 'Value', 0).should('contain.text', 'Hull Court')
-    page
-      .getCardHeader('conviction')
-      .eq(1)
-      .within(() => cy.get('.govuk-summary-list__value').eq(2).should('contain.text', 'No conviction date'))
-
-    page.getRowDataIndex('sentence', 'orderDescription', 'Value', 0).should('contain.text', 'Default Sentence Type')
-    page.getRowDataIndex('sentence', 'orderStartDate', 'Value', 0).should('contain.text', '19 March 2024')
-    page.getRowDataIndex('sentence', 'orderEndDate', 'Value', 0).should('contain.text', '19 March 2025')
+    page.getRowData('offence', 'mainOffence', 'Value').should('contain.text', 'Murder (3 count)')
+    page.getRowData('offence', 'dateOfOffence', 'Value').should('contain.text', '20 March 2024')
+    page.getRowData('offence', 'offenceNotes', 'Value').should('contain.text', 'overview')
+    page.getRowData('offence', 'additionalOffences', 'Value').should('contain.text', 'Burglary (2 count)')
+    page.getRowData('offence', 'additionalOffences', 'Value').should('contain.text', 'Assault (1 count)')
+    page.getRowData('sentence', 'orderDescription', 'Value').should('contain.text', 'Default Sentence Type')
+    page.getRowData('sentence', 'orderStartDate', 'Value').should('contain.text', '19 March 2024')
+    page.getRowData('sentence', 'orderEndDate', 'Value').should('contain.text', '19 March 2025')
 
     page
-      .getRowDataIndex('sentence', 'courtDocuments', 'Value', 0)
+      .getRowData('sentence', 'courtDocuments', 'Value')
       .within(() => cy.get('ul > li').its('length').should('equal', 3))
 
     page
-      .getRowDataIndex('sentence', 'courtDocuments', 'Value', 0)
+      .getRowData('sentence', 'courtDocuments', 'Value')
       .within(() =>
         cy
           .get('ul > li:first')
@@ -83,13 +58,13 @@ context('Sentence', () => {
           ),
       )
     page
-      .getRowDataIndex('sentence', 'courtDocuments', 'Value', 0)
+      .getRowData('sentence', 'courtDocuments', 'Value')
       .within(() => cy.get('ul > li:first').should('contain.text', 'Pre-sentence report'))
     page
       .getRowDataIndex('sentence', 'courtDocuments', 'Value', 0)
       .within(() => cy.get('ul > li:first').should('contain.text', 'Last updated 3 Apr 2024'))
 
-    page.getRowDataIndex('sentence', 'courtDocuments', 'Value', 0).within(() =>
+    page.getRowData('sentence', 'courtDocuments', 'Value').within(() =>
       cy
         .get('ul > li')
         .eq(1)
@@ -101,13 +76,13 @@ context('Sentence', () => {
         ),
     )
     page
-      .getRowDataIndex('sentence', 'courtDocuments', 'Value', 0)
+      .getRowData('sentence', 'courtDocuments', 'Value')
       .within(() => cy.get('ul > li').eq(1).should('contain.text', 'CPS Pack'))
     page
-      .getRowDataIndex('sentence', 'courtDocuments', 'Value', 0)
+      .getRowData('sentence', 'courtDocuments', 'Value')
       .within(() => cy.get('ul > li').eq(1).should('contain.text', 'Last updated 1 Apr 2024'))
 
-    page.getRowDataIndex('sentence', 'courtDocuments', 'Value', 0).within(() =>
+    page.getRowData('sentence', 'courtDocuments', 'Value').within(() =>
       cy
         .get('ul > li')
         .eq(2)
@@ -119,29 +94,10 @@ context('Sentence', () => {
         ),
     )
     page
-      .getRowDataIndex('sentence', 'courtDocuments', 'Value', 0)
+      .getRowData('sentence', 'courtDocuments', 'Value')
       .within(() => cy.get('ul > li').eq(2).should('contain.text', 'Previous convictions'))
     page
-      .getRowDataIndex('sentence', 'courtDocuments', 'Value', 0)
+      .getRowData('sentence', 'courtDocuments', 'Value')
       .within(() => cy.get('ul > li').eq(2).should('contain.text', 'Unavailable'))
-
-    page
-      .getCardHeader('probationHistory')
-      .within(() => cy.get('.govuk-summary-list__key').eq(0).should('contain.text', 'Previous orders'))
-    page
-      .getCardHeader('probationHistory')
-      .within(() => cy.get('.govuk-summary-list__key').eq(1).should('contain.text', 'Previous breaches'))
-    page
-      .getCardHeader('probationHistory')
-      .within(() => cy.get('.govuk-summary-list__key').eq(2).should('contain.text', 'Previous professional contacts'))
-    page
-      .getCardHeader('probationHistory')
-      .within(() => cy.get('.govuk-summary-list__value').eq(1).should('contain.text', '2 previous breaches'))
-    page
-      .getCardHeader('probationHistory')
-      .within(() => cy.get('a').eq(0).invoke('attr', 'href').should('equal', '/case/X000001/sentence/previous-orders'))
-    page
-      .getCardHeader('probationHistory')
-      .within(() => cy.get('a').eq(1).invoke('attr', 'href').should('equal', '/case/X000001/address-book-professional'))
   })
 })

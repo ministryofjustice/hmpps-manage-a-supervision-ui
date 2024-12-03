91a5a1502c419f2b75bc803db628573c32180e9d
Neil Mills <neil.mills@digital.justice.gov.uk>
2024-12-03 17:24:10 +0000

    MAN-183 add timepicker package

--- assets/js/mojFrontendInit.js
+++ assets/js/mojFrontendInit.js
@@ -1,4 +1,5 @@
 window.MOJFrontend.initAll()
+$('.time-field').timepicker({ minTime: '9:00AM', maxTime: '4:45PM', step: 15 })
 const $backendSortableTables = document.querySelectorAll('[data-module="moj-backend-sortable-table"]')
 MOJFrontend.nodeListForEach($backendSortableTables, function ($table) {
   const best = new MOJFrontend.BackendSortableTable({

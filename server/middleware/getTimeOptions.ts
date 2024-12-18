74a6680ecc85bc8511076b08a4f41aa47cce480a
Neil Mills <neil.mills@digital.justice.gov.uk>
2024-12-18 16:26:29 +0000

    MAN-183 fix sonar code issues

--- server/middleware/getTimeOptions.ts
+++ server/middleware/getTimeOptions.ts
@@ -13,9 +13,9 @@ export const getTimeOptions: Route<void> = (_req, res, next) => {
     const hour = i > 12 ? i - 12 : i
     const suffix = i >= 12 ? 'pm' : 'am'
     timeOptions.push({ text: `${hour}:00${suffix}` })
-    for (let index = 1; index < 4; index += 1) {
-      timeOptions.push({ text: `${hour}:${index * 15}${suffix}` })
-    }
+    ;['15', '30', '45'].forEach(min => {
+      timeOptions.push({ text: `${hour}:${min}${suffix}` })
+    })
   }
   res.locals.timeOptions = timeOptions
   return next()

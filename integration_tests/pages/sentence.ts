ec7551749518828113f5c27477607c92e054be8f
Neil Mills <neil.mills@digital.justice.gov.uk>
2024-12-20 09:21:02 +0000

    MAN-225 integration test for no active sentence

--- integration_tests/pages/sentence.ts
+++ integration_tests/pages/sentence.ts
@@ -1,7 +1,11 @@
-import Page from './page'
+import Page, { PageElement } from './page'
 
 export default class SentencePage extends Page {
   constructor() {
     super('Sentence')
   }
+
+  activeSideNavItem = (): PageElement => cy.get('.moj-side-navigation__item--active a')
+
+  noActiveSentence = (): PageElement => cy.get('[data-qa="no-active-sentence"]')
 }

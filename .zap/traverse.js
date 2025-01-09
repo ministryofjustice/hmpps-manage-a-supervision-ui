1e38df6e8b5d4a144333ce7456dc08bb2780bd67
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2025-01-09 12:04:46 +0000

    MAN-227 - update js script to put variable declaration in for loop

--- .zap/traverse.js
+++ .zap/traverse.js
@@ -1,6 +1,5 @@
 function listChildren(node, level) {
-  var j
-  for (j = 0; j < node.getChildCount(); j++) {
+  for (let j = 0; j < node.getChildCount(); j++) {
     print(Array(level + 1).join('    ') + node.getChildAt(j).getNodeName())
     listChildren(node.getChildAt(j), level + 1)
   }

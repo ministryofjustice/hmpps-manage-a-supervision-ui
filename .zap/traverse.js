5d8bdcbc66474e48c33b9678882595c009238ed0
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2025-01-09 11:48:20 +0000

    MAN-227 - update js script back to previous version, due to failure in github action

--- .zap/traverse.js
+++ .zap/traverse.js
@@ -1,5 +1,5 @@
 function listChildren(node, level) {
-  let j
+  var j
   for (j = 0; j < node.getChildCount(); j++) {
     print(Array(level + 1).join('    ') + node.getChildAt(j).getNodeName())
     listChildren(node.getChildAt(j), level + 1)

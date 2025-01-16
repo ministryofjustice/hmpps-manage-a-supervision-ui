4f7feb1bebd46e289b5eed8823b460539d1f9fea
Neil Mills <neil.mills@digital.justice.gov.uk>
2025-01-16 21:16:43 +0000

    MAN-82 working draft of filter menu functionality

--- server/routes/activityLog.ts
+++ server/routes/activityLog.ts
@@ -1,28 +1,116 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-nested-ternary */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable consistent-return */
+/* eslint-disable no-console */
 import { type RequestHandler, Router } from 'express'
 import { auditService } from '@ministryofjustice/hmpps-audit-client'
 import { v4 } from 'uuid'
-// eslint-disable-next-line import/no-extraneous-dependencies
+
 import { Query } from 'express-serve-static-core'
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
 import MasApiClient from '../data/masApiClient'
 import TierApiClient from '../data/tierApiClient'
 
+interface Filters {
+  keywords: string
+  dateFrom: string
+  dateTo: string
+  compliance: string[] | string
+}
+
 export default function activityLogRoutes(router: Router, { hmppsAuthClient }: Services) {
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/case/:crn/activity-log', async (req, res, _next) => {
     const { crn } = req.params
+    const { url } = req
     const token = await hmppsAuthClient.getSystemClientToken(res.locals.user.username)
     const masClient = new MasApiClient(token)
     const tierClient = new TierApiClient(token)
+    const { keywords = '', dateFrom = '', dateTo = '', clearFilterKey, clearFilterValue } = req.query
+    let { compliance } = req.query
+    const baseUrl = `/case/${crn}/activity-log`
+
+    compliance = compliance ? (Array.isArray(compliance) ? compliance : [compliance]) : []
+    if (compliance?.length && clearFilterKey === 'compliance') {
+      compliance = compliance.filter(value => value !== clearFilterValue)
+    }
+
+    const filters: Filters = {
+      keywords: keywords && clearFilterKey !== 'keywords' ? (keywords as string) : '',
+      dateFrom: dateFrom && clearFilterKey !== 'dateRange' ? (dateFrom as string) : '',
+      dateTo: dateTo && clearFilterKey !== 'dateRange' ? (dateTo as string) : '',
+      compliance: compliance as string[],
+    }
+
+    const queryStr = Object.entries(filters).reduce((acc, [key, value], i) => {
+      if (value) {
+        if (Array.isArray(value)) {
+          for (let j = 0; j < value.length; j += 1) {
+            acc = `${acc}${acc ? '&' : ''}${key}=${encodeURI(value[j])}`
+          }
+        } else {
+          acc = `${acc}${i > 0 ? '&' : ''}${key}=${encodeURI(value)}`
+        }
+      }
+      return acc
+    }, '')
+
+    if (clearFilterKey) {
+      return res.redirect(`${baseUrl}${queryStr ? `?${queryStr}` : ''}`)
+    }
+
+    const filterHref = (key: string, value: string): string =>
+      `${baseUrl}${queryStr ? `?${queryStr}&` : '?'}&clearFilterKey=${key}&clearFilterValue=${encodeURI(value)}`
+
+    const selectedFilterItems = Object.entries(filters)
+      .filter(([_key, value]) => value)
+      .reduce((acc, [key, value]) => {
+        if (Array.isArray(value)) {
+          for (const text of value) {
+            acc = [
+              ...acc,
+              {
+                text,
+                href: filterHref(key, text),
+              },
+            ]
+          }
+        } else if (key !== 'dateTo') {
+          let text = value
+          let cfKey = key
+          if (key === 'dateFrom') {
+            text = `${value} - ${filters.dateTo}`
+            cfKey = 'dateRange'
+          }
+          acc = [
+            ...acc,
+            {
+              text,
+              href: filterHref(cfKey, value),
+            },
+          ]
+        }
+        return acc
+      }, [])
+
+    if (selectedFilterItems.length) {
+      console.log('request activity log results...')
+    }
+
+    const complianceOptions = ['Absences waiting for evidence', 'Acceptable absences', 'Appointments'].map(option => ({
+      text: option,
+      value: option,
+      checked: filters.compliance.includes(option),
+    }))
 
     if (req.query.view === 'compact') {
       res.locals.compactView = true
     } else {
       res.locals.defaultView = true
     }
-
     if (req.query.requirement) {
       res.locals.requirement = req.query.requirement
     }
@@ -47,6 +135,12 @@ export default function activityLogRoutes(router: Router, { hmppsAuthClient }: S
       crn,
       queryParams,
       tierCalculation,
+      filters: {
+        selectedFilterItems,
+        complianceOptions,
+        baseUrl,
+        ...filters,
+      },
     })
   })
 

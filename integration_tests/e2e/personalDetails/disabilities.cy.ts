ae8916c1dcde16bf2d6fda06f387470efcf2e52c
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2025-02-06 09:06:55 +0000

    MAN-372 add missing header to disabilities page

--- integration_tests/e2e/personalDetails/disabilities.cy.ts
+++ integration_tests/e2e/personalDetails/disabilities.cy.ts
@@ -1,17 +1,16 @@
+import DisabilityPage from '../../pages/personalDetails/disability'
+import Page from '../../pages/page'
+
 context('Contact', () => {
   it('Contact page is rendered', () => {
     cy.visit('/case/X000001/personal-details/disabilities')
 
-    cy.get('p')
-      .eq(0)
-      .within(() =>
-        cy
-          .get('a')
-          .invoke('attr', 'href')
-          .should(
-            'equal',
-            'https://ndelius-dummy-url/NDelius-war/delius/JSP/deeplink.xhtml?component=EqualityMonitoring&CRN=X000001',
-          ),
-      )
+    const page = Page.verifyOnPage(DisabilityPage)
+    page.assertAnchorElementAtIndexWithin(
+      'p',
+      0,
+      0,
+      'https://ndelius-dummy-url/NDelius-war/delius/JSP/deeplink.xhtml?component=EqualityMonitoring&CRN=X000001',
+    )
   })
 })

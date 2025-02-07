0204974ce385fca7953d406ad15652a87c222884
Neil Mills <neil.mills@digital.justice.gov.uk>
2025-02-07 10:45:53 +0000

    MAN-324 add risk level wip

--- server/routes/risks.ts
+++ server/routes/risks.ts
@@ -37,7 +37,7 @@ export default function risksRoutes(router: Router, { hmppsAuthClient }: Service
       arnsClient.getPredictorsAll(crn),
       arnsClient.getNeeds(crn),
     ])
-
+    console.log(personRisk)
     let timeline: TimelineItem[] = []
     let predictorScores
     if (Array.isArray(predictors)) {
